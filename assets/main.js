5f4411f29f4d670b4309cbe5b270a830c75d237a
balcoder <des.barrett@gmail.com>
2024-03-08 09:14:08 +0000

    added js to main for img creation

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+const img1 = document.getElementById('foo');
+const img = new Image();
+img.src = "./img/hotel-1.jpg";
+img.setAttribute('width', '50%');
+img.setAttribute('height', '50%');
+img1.appendChild(img);
+
