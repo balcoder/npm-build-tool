b2ecd9b5964902b19199b9b44977891d1a72707d
balcoder <des.barrett@gmail.com>
2024-05-23 13:46:11 +0100

    updates vunerable dependencies

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
 console.log('test 1');
 console.log('test 1.1');
-const h1 = document.getElementById("foo")
-
-h1.innerText = "Text added from main.js"
+const img1 = document.getElementById('foo');
+const img = new Image();
+img.src = "./img/hotel-1.jpg";
+img.setAttribute('width', '50%');
+img.setAttribute('height', '50%');
+img1.appendChild(img);
